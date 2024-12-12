265f0b57816cae84f9de6f98b5efddf76bae4cbb
YEN\ADMIN <hoangyen2023>
2024-12-12 20:34:07 +0700

    Add project files.

---
+++ WebBanSon/Models/ViewModel/MessageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebBanSon.Models.ViewModel
+{
+    public class MessageModel
+    {
+        public string FromUser { get; set; }
+        public string ToUser { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ WebBanSon/Services/IVnPayService.cs
@@ -0,0 +1,10 @@
+﻿using WebBanSon.ViewModel;
+
+namespace WebBanSon.Services
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(HttpContext context, VnPaymentRequestModel model);
+        VnPaymentResponseModels PaymentExecute(IQueryCollection collections);
+    }
+}
